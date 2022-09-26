dfcf3fbf9cd432f274467f24b04207b64f261f68
Rdz-Daniela <114102955+Rdz-Daniela@users.noreply.github.com>
2022-09-26 14:06:37 -0500

    Add files via upload

---
+++ Consultas/Consultas/AccionesComunes.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace Consultas
+{
+    internal class AccionesComunes
+    {
+        public static void Mensaje(string mensaje)
+        {
+            MessageBox.Show(mensaje, "AVISO!!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public static void LlenarCombo(String consulta, ComboBox combo, string id, string campo)
+        {
+            DataTable dt;
+            dt = Conexion.EjecutaSeleccion(consulta);
+
+            DataRow r = dt.NewRow();
+            r[0] = 0;
+            r[1] = "Todos Los Datos";
+            dt.Rows.InsertAt(r, 0);
+
+            if (dt== null)
+            {
+                return;
+            }
+            combo.Items.Clear();
+            combo.DataSource = dt;
+            combo.ValueMember = id;
+            combo.DisplayMember= campo;
+        }
+        public static void LlenarData (string consulta, DataGridView data)
+        {
+            DataTable dt;
+            dt = Conexion.EjecutaSeleccion(consulta);
+            if (dt == null)
+            {
+                return;
+            }
+            data.DataSource = dt;
+        }
+        public static void LlenarList(string consulta, ListView lista)
+        {
+            DataTable dt;
+            dt = Conexion.EjecutaSeleccion(consulta);
+            int columna = dt.Columns.Count;
+            if (dt == null)
+            {
+                return;
+            }
+            lista.View = View.Details;
+            foreach (DataColumn itemColumn in dt.Columns)
+            {
+                lista.Columns.Add(Convert.ToString(itemColumn));
+            }
+            foreach (DataRow row in dt.Rows)
+            {
+                ListViewItem itemlist = new ListViewItem(Convert.ToString(row[0]));
+                for (int i = 1; i < columna; i++)
+                {
+                    itemlist.SubItems.Add(Convert.ToString(row[i]));
+                }
+                lista.Items.Add(itemlist);
+            }
+        }
+    }
+    }
+
---
+++ Consultas/Consultas/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consultas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Registros = 0;
+            Conexion.EjecutaConsulta(textBox1.Text);
+            AccionesComunes.Mensaje("" + Registros);
+            AccionesComunes.LlenarCombo(textBox1.Text, comboBox1, "id_Cliente", "Nombre_Cliente");
+            AccionesComunes.LlenarData(textBox1.Text, dataGridView1);
+            AccionesComunes.LlenarList(textBox1.Text, listView1);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.SelectedValue.ToString();
+        }
+    }
+}
